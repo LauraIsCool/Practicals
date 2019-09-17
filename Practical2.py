ea5ad51bc7170f022e736048d9a339c1195eab65
Laura <pembertonlaura35@gmail.com>
2019-09-17 10:25:06 +0100

    finished practical 2

--- Practical2.py
+++ Practical2.py
@@ -7,105 +7,101 @@ Created on Mon Sep 16 15:11:11 2019
 """
 
 """ 
-Practical 1 Assignment: Agent Based Modelling
-This program creates two agent coordinates and randomly assignes the x and y value 
-of an integer between 1 and 100.
+Practical 2 Assignment: Code shrinking 1
 
-Next the program generates another random number that is used to determine how the 
-x and y coordinates for each agent will move. 
+This code uses the array function to create x and y coordinates for both agents
+the code then increments each of the integers within the array as the code from 
+practical 1 did. 
+ 
 
-If the random number generated is less than 0.5 then the coordinate will move
-either up the y axis 1 point or right across the x axis one point. 
-
-Finally the programme takes the newly calculated coordinates and calculates the 
-distance between them using the square root function. 
-
-The programme then outputs the coordinates for both agents and the distance 
-between them.
 """
 
 import random #import random module
+import array 
+import operator
+import matplotlib.pyplot
 
-random_numberY_Axis = random.random() #this value determines the random value of the y axis movement
-random_numberX_Axis = random.random() 
+agents = []
 
-
-y0 = random.randint(0,99)
-x0 = random.randint(0,99)
-
-
-
-#First Iteration
-if random_numberY_Axis < 0.5: """this if function states if the random number 
-generated is less than 0.5 move y0 uthe y axis one place"""
-    y0 = y0 + 1
-else: #this else statement states that if the random number is more than 0.5 
-    y0 = y0 - 1 
-    
-
-if random_numberX_Axis < 0.5:
-    x0 = x0 + 1
-else:
-    x0 = x0 - 1
-
-#Second Iteration
-if random_numberY_Axis < 0.5:
-    y0 = y0 + 1
-else:
-    y0 = y0 - 1
-    
-
-
-if random_numberX_Axis < 0.5:
-    x0 = x0 + 1
-else:
-    x0 = x0 - 1
-    
-    
-#Output
-print ("first agent coordinates:")
-print ("Random y0 = ", y0)
-print ("Random x0 = ", x0)
-
-
-"""Second Agent"""
 random_numberY_Axis = random.random() 
-random_numberX_Axis = random.random()
+random_numberX_Axis = random.random() 
 
 
 y1 = random.randint(0,99)
 x1 = random.randint(0,99)
 
 
+agents.append([random.randint(0,99), random.randint(0,99)])
+
+print("random agent coordinates 0:", agents[0])
+
 #First Iteration
-if random_numberY_Axis < 0.5:
-    y1 = y1 + 1
-else:
-    y1 = y1 - 1
+if random_numberY_Axis < 0.5: 
+    agents[0][0] += 1
+else: #this else statement states that if the random number is more than 0.5 
+    agents[0][0] -= 1 
     
 
 if random_numberX_Axis < 0.5:
-    x1 = x1 + 1
+    agents[0][1] += 1
 else:
-    x1 = x1 - 1
+    agents[0][1] -= 1
 
 #Second Iteration
 if random_numberY_Axis < 0.5:
-    y1 = y1 + 1
+    agents[0][0] += 1
 else:
-    y1 = y1 - 1
+    agents[0][0] -= 1 
+    
+
+
+if random_numberX_Axis < 0.5:
+    agents[0][1] += 1
+else:
+    agents[0][1] -= 1
+    
+    
+#Output
+print ("first agent coordinates:")
+print ("Random y0 = ", agents[0][0])
+print ("Random x0 = ", agents[0][1])
+
+
+"""Second Agent"""
+
+agents.append([random.randint(0,99), random.randint(0,99)])
+
+print("random agent coordinates 0:", agents[1])
+
+#First Iteration
+if random_numberY_Axis < 0.5:
+    agents[1][0] += 1
+else:
+    agents[1][0] -= 1
     
 
 if random_numberX_Axis < 0.5:
-    x1 = x1 + 1
+    agents[1][1] += 1
 else:
-    x1 = x1 - 1
+    agents[1][1] -= 1
+
+#Second Iteration
+if random_numberY_Axis < 0.5:
+    agents[1][0] += 1
+else:
+    agents[1][0] -= 1
+    
+
+if random_numberX_Axis < 0.5:
+    agents[1][1] += 1
+else:
+    agents[1][1] -= 1
     
     
 #Output
 print ("second agent coordinates:")
-print ("Random y1 = ", y1)
-print ("Random x1 = ", x1)
+print ("Random y1 = ", agents[1][0])
+print ("Random x1 = ", agents[1][1])
 
 
 """ Calculate distance between both agents """
@@ -113,7 +109,21 @@ print ("Random x1 = ", x1)
 import math
 
 
-distance = math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+distance = math.sqrt((agents[1][1] - agents[0][1])**2 
+                     + (agents[1][0] - agents[0][0])**2)
 print ("distance between coordinates:")
 
 print (round(distance)) 
+
+
+""" Working with functions"""
+print ("largest x value:", max(agents, key=operator.itemgetter(1)))
+
+""" external library used to plot data in graphs"""
+matplotlib.pyplot.ylim(0,100)
+matplotlib.pyplot.xlim(0,100)
+matplotlib.pyplot.scatter(agents[0][1], agents[0][0])
+matplotlib.pyplot.scatter(agents[1][1], agents[1][0])
+m = max(agents, key=operator.itemgetter(1)) 
+matplotlib.pyplot.scatter(m[1], m[0], color='red')
+matplotlib.pyplot.show()
