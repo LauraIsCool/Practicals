4771fe4b90d2c0c13316c40c660c90af4cf74e3f
Laura <pembertonlaura35@gmail.com>
2019-09-17 17:06:03 +0100

    started practical 5

---
+++ agentframework5.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 15:40:22 2019
+
+@author: laurapemberton
+"""
+class Agent():
+    
+    def __init__(self, agents):
+        pass
+
+
+
+def move():
+    if random.random() < 0.5:
+        agents[i][0] = (agents[i][0] + 1) % 100
+    else:
+        agents[i][0] = (agents[i][0] - 1) % 100
+
+    if random.random() < 0.5:
+        agents[i][1] = (agents[i][1] + 1) % 100
+    else:
+        agents[i][1] = (agents[i][1] - 1) % 100
+            
+
+           
+        
