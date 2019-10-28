4f3f35cfc908237bf88710308e6b2eaec6f4088b
Laura Pemberton <pembertonlaura35@gmail.com>
2019-10-28 11:37:47 +0000

    commit pls

---
+++ Practical9Working - final.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 15:29:39 2019
+
+@author: laurapemberton
+
+
+Practical 9: GUI
+
+Sections of code taken from: https://www.geog.leeds.ac.uk/courses/computing/study/core-python-phd/
+
+
+"""
+import matplotlib
+matplotlib.use('TkAgg')
+import tkinter
+import matplotlib.pyplot
+import agentframework9final
+import random
+import matplotlib.animation
+import matplotlib.backends
+
+import requests
+import bs4
+
+
+# =============================================================================
+# declare all variables
+# =============================================================================
+num_of_agents = 100
+num_of_iterations = 50
+neighbourhood = 20
+
+# =============================================================================
+# Create lists for environment and agents.
+# =============================================================================
+agents = []  
+environment = []
+
+# =============================================================================
+# Code here used to scrape data off the internet 
+# =============================================================================
+
+r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html')
+content = r.text
+soup = bs4.BeautifulSoup(content, 'html.parser')
+td_ys = soup.find_all(attrs={"class" : "y"})
+td_xs = soup.find_all(attrs={"class" : "x"})
+print(td_ys)
+print(td_xs)
+
+# =============================================================================
+# #read text file and create environment          
+# =============================================================================
+f = open("in.txt")
+for line in f:  
+    parsed_line = str.split(line, ",") 
+    data_line = [] 
+    for word in parsed_line: 
+        data_line.append(float(word))      
+    environment.append(data_line)
+#print(data) 
+#Test print statement. 
+f.close() 
+
+
+# =============================================================================
+# create figure animation
+# =============================================================================
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+
+
+#ax.set_autoscale_on(False)
+
+def run():
+    animation = matplotlib.animation.FuncAnimation(fig, update, 
+                                                   frames=gen_function, 
+                                                   repeat= False) 
+
+    
+    #canvas.show()
+    canvas.draw()
+
+# =============================================================================
+# Make the agents.
+# =============================================================================
+for i in range(num_of_agents):
+    y = int(td_ys[i].text)
+    x = int(td_xs[i].text)
+    agents.append(agentframework9final.Agent(environment, agents, y, x)) 
+
+carry_on = True
+
+def update(frame_number):
+    fig.clear()
+    global carry_on     
+        
+    # =========================================================================
+    # Move the agents.
+    # =========================================================================
+    for j in range(num_of_iterations):
+        random.shuffle(agents)
+        for i in range(num_of_agents):
+            agents[i].move()
+            #print(agents[i].x) 
+            #Print statement used as test case to confirm agents have moved. 
+                                 
+    # =========================================================================
+    # Call methods that do stuff to agents.    
+    # =========================================================================
+    for j in range(num_of_iterations):
+        for i in range(num_of_agents):
+            agents[i].move()
+            agents[i].eat()
+            agents[i].share_with_neighbours(neighbourhood)
+            agents[i].throwup()
+            #agents[i].sheep_in_hole()
+
+            #agents[i].sheep_in_hole(agent)
+
+    # =========================================================================
+    # #Create animated graphic
+    # =========================================================================
+    matplotlib.pyplot.xlim(0,300)
+    matplotlib.pyplot.ylim(0, 300)
+    matplotlib.pyplot.imshow(environment) 
+    for i in range(num_of_agents):
+        matplotlib.pyplot.scatter(agents[i].x,agents[i].y, c='white')
+        #print("agent ", agents[i], "coordinates: ", agents[i].x, agents[i].y)
+        #print statement shows coordinates for generated agents. 
+
+#supplied generator function to frame number.           
+def gen_function(b = [0]):
+    a = 0
+    global carry_on
+    while (a<num_of_iterations) & (carry_on):
+        yield a
+        a = a + 1
+        
+root = tkinter.Tk()
+root.wm_title("Model")
+canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1) 
+
+# =============================================================================
+# #Code to make menu
+# =============================================================================
+menu_bar = tkinter.Menu(root)
+root.config(menu=menu_bar)
+model_menu = tkinter.Menu(menu_bar)
+menu_bar.add_cascade(label='Model', menu=model_menu)
+model_menu.add_command(label="Run model", command = run)
+
+
+tkinter.mainloop()
+
+
+
+""" END OF PROGRAM. """
+"""
+
+aTTEMPT TO WRITE FUNCTION THAT WOULD ENABLE THE PROGRAM TO BE PAUSED BY THE USER. 
+could not find a function within python library i could understand to implement
+this. 
+def pause():
+    programPause = input()
+
+
+attempt to write code that could pause the program. Could not find function
+in library that would do this
+model_menu.add_command(label="Pause model", command = pause)   
+
+import time 
+start = time.process_time()
+end = time.process_time()
+print ("time completed in: "+ str(round(end-start, 3)))                                       
+
+"""
+        
+        
---
+++ agentframework9final.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 15:40:22 2019
+
+@author: laurapemberton
+
+Practical 9: Agent class 
+
+This class is representative of the agent. The module creates the agent and 
+then values are assigned to the agent within this class. 
+All agents are created with a random x and y variable. 
+
+A function (move) is created to move the agents twice for both the x and y value 
+randomly. 
+
+A function (eat) is used 
+
+A function (share with neighbours)
+
+A function (distance_between) returns the value of the distance between two 
+given agents. 
+    
+    
+"""
+
+import random
+
+class Agent():
+
+    def __init__(self, environment, list_agents, y, x): #sheep_in_hole
+        self.x = x
+        self.y = y
+        self.environment = environment
+        self.store = 0
+        self.agents = list_agents
+
+
+    def move(self):
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 300
+        else:
+            self.x = (self.x - 1) % 300
+    
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 300
+        else:
+            self.y = (self.y - 1) % 300
+            
+           
+    
+    def eat(self):
+        if self.environment[self.y][self.x] > 10:
+            self.environment[self.y][self.x] -=10
+            self.store += 10
+
+    def share_with_neighbours(self, neighbourhood):
+        for agent in self.agents:
+            dist = self.distance_between(agent)
+            if dist <= neighbourhood:
+                sum = self.store + agent.store
+                average = sum/2
+                self.store = average
+                agent.store = average
+            
+                #print("sharing " + str(dist) + " " + str(ave))
+        #print(neighbourhood)
+        #print statement to test that share with neighbour function works. 
+    
+    def distance_between(self, agent):
+        return(((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
+
+    #  When an agent has a store greater than 100, the agent throws up their  
+    # store in current location
+    def throwup(self):
+        if self.store >= 100:            
+            self.environment[self.y][self.x] +=100
+            self.store = self.store-100  
+            
+""" END OF CLASS """            
+# =============================================================================
+# Code written in attempt to get agents to "fall" in a hole thats in the middle
+# of the grid. However could not get this to work. Could not work out how to 
+# take sheep in hole list from agents list within main program document. 
+
+        
+# =============================================================================
+
+#     def fall_in_hole(self, agents):
+#         sheep_in_hole = []
+#         if self.x > 145 and self.x < 156 and self.y > 145 and self.y < 156:
+#              sheep_in_hole.append(self.x, self.y)
+#              agents.remove(sheep_in_hole)
+#         return agents
+# =============================================================================
+
+        
+    
+        
